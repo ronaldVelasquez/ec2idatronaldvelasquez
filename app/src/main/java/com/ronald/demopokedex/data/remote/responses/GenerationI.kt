9167c6c1dfdb55b8973218d73f03dc9c3ebd9ed1
Ronald Velasquez <ronaldvg93@gmail.com>
2023-07-05 11:49:51 -0500

    Agregar apk

--- app/src/main/java/com/ronald/demopokedex/data/remote/responses/GenerationI.kt
+++ app/src/main/java/com/ronald/demopokedex/data/remote/responses/GenerationI.kt
@@ -1,6 +1,9 @@
 package com.ronald.demopokedex.data.remote.responses
 
+import com.google.gson.annotations.SerializedName
+
 data class GenerationI(
-    val red-blue: RedBlue,
+    @SerializedName("red-blue")
+    val redblue: RedBlue,
     val yellow: Yellow
 )
